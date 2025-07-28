3a916a73cac06e7df67e4f9a61db65a06943cef5
sphumelele ndlovu <sphumelelendlovu26@gmail.com>
2025-07-28 11:53:22 +0200

    added a few functional buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import TaskComponent from "./components/TaskComponent";
+import { FaFilter } from "react-icons/fa";
 
 function App() {
   const [userInput, setUserInput] = useState("");
   const [tasks, setTasks] = useState([]);
+  const [editText, setEditText] = useState("");
+  const [favFilter, setFavFilter] = useState("All");
 
   useEffect(() => {
     document.querySelector("input")?.focus();
   }, []);
-
-  useEffect(() => {
-    console.log(tasks);
-  }, [tasks]);
 
   useEffect(() => {
     const savedTasks = localStorage.getItem("tasks");
@@ -32,11 +31,43 @@ function App() {
   function addTasks() {
     const date = new Date();
     const id = Math.floor(date - Math.random() * 1000);
+
     if (userInput.trim() !== "") {
-      setTasks([...tasks, { text: userInput, id: id }]);
-      userInput("");
+      setTasks([
+        ...tasks,
+        { text: userInput, id: id, isFavorite: false, isEditing: false },
+      ]);
+      setUserInput("");
     }
   }
+
+  function toggleFavorite(favoriteId) {
+    setTasks((prev) =>
+      prev.map((task) =>
+        task.id === favoriteId
+          ? { ...task, isFavorite: !task.isFavorite }
+          : task
+      )
+    );
+  }
+  function toggleTaskEditing(idToEdit) {
+    setTasks((prev) =>
+      prev.map((task) =>
+        task.id === idToEdit ? { ...task, isEditing: !task.isEditing } : task
+      )
+    );
+    const taskToEdit = tasks.find((task) => task.id === idToEdit);
+    if (taskToEdit) setEditText(taskToEdit.text);
+  }
+  function saveEdit(id) {
+    setTasks((prev) =>
+      prev.map((task) =>
+        task.id === id ? { ...task, text: editText, isEditing: false } : task
+      )
+    );
+    setEditText("");
+  }
+
   function deleteTask(idToBeDeleted) {
     const tasksAfterDel = tasks.map((task) =>
       task.id === idToBeDeleted ? { ...task, deleting: true } : task
@@ -47,6 +78,7 @@ function App() {
       setTasks((prev) => prev.filter((task) => task.id !== idToBeDeleted));
     }, 300);
   }
+
   function clearTasks() {
     const tasksMarked = tasks.map((task) => ({ ...task, deleting: true }));
     setTasks(tasksMarked);
@@ -55,58 +87,91 @@ function App() {
     }, 300);
   }
 
+  function filterTasks() {}
+
   return (
-    <div className="slide-in-right bg-zinc-700 text-gray-100 overflow-x-hidden   h-screen p-5 ">
-      <header className="static flex  flex-col  items-center w-full px-4">
-        <h1 className="text-4xl font-bold mb-4 text-white">
+    <div className="slide-in-right bg-zinc-700 text-gray-100 min-h-screen p-5 px-4 sm:px-8 overflow-x-hidden">
+      {/*inputs*/}
+      <header className="flex flex-col items-center w-full max-w-3xl mx-auto text-center">
+        <h1 className="text-3xl sm:text-4xl font-bold mb-4 text-white">
           What’s on your mind today?
         </h1>
-        <p className="text-lg mb-6 text-white-700 italic">
+        <p className="text-base sm:text-lg mb-6 text-gray-300 italic">
           Every idea starts with a single task.
         </p>
-        <div className="flex space-x-2"></div>
-        <div className="flex md:flex-row flex-col flex-grow gap-2 w-full max-w-xl  ">
+
+        <div className="flex flex-col md:flex-row gap-2 w-full">
+          {/*Task Input*/}
           <input
-            className="outline-indigo-600 border-none bg-zinc-500 p-4  w-full text-white rounded"
+            className="outline-indigo-600 border-none bg-zinc-500 p-4 text-white rounded w-full"
             placeholder="Enter Task"
             type="text"
             value={userInput}
             onChange={(e) => setUserInput(e.target.value)}
             onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                addTasks();
-              }
+              if (e.key === "Enter") addTasks();
             }}
           />
+          {/* Add Task Button */}
+
           <button
-            className="bg-indigo-600 w-full sm:w-auto hover:text-green-500  hover:bg-indigo-700 rounded transition"
-            onClick={() => addTasks()}
+            className="bg-indigo-600 px-4 py-2 text-white rounded hover:text-green-500 hover:bg-indigo-700 transition w-full md:w-auto"
+            onClick={addTasks}
           >
             Add Task
           </button>
         </div>
-      </header>{" "}
+        <nav></nav>
+      </header>
+
       {tasks.length > 0 && (
-        <div className="slide-in-right mt-8 w-full max-w-2xl mx-auto">
-          <h3 className="flex  justify-between">
-            <span className="border-4 w-auto rounded border-indigo-600 p-1">
+        <main className="slide-in-right mt-10 w-full max-w-3xl mx-auto px-1">
+          <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 mb-4">
+            <span className="border-4 rounded border-indigo-600 px-3 py-1 text-white text-lg">
               All Tasks
             </span>
-            <button
-              className="bg-indigo-600 w-50 hover:bg-indigo-700 rounded   hover:text-red-500 transition duration-300"
-              onClick={() => clearTasks()}
-            >
-              clear Tasks
-            </button>
-          </h3>
-          <ul className="p-3 border-2 border-indigo-600 mt rounded-lg ">
+            {/*Clear all tasks button*/}
+            <div className="flex justify-evenly ">
+              <select
+                id="filter"
+                className="bg-indigo-600 mr-2 text-white px-2 py-1 rounded focus:outline-none transition"
+              >
+                <option value="All" className="hover:bg-indigo-700">
+                  All
+                </option>
+                <option value="Favorites" className="hover:bg-indigo-700">
+                  Favorites
+                </option>
+                <option value="Completed" className="hover:bg-indigo-700">
+                  Completed
+                </option>
+              </select>
+              <button
+                className="bg-indigo-600 px-4 py-2 rounded text-white hover:bg-indigo-700 hover:text-red-500 transition duration-300"
+                onClick={clearTasks}
+                title="Clear All Tasks"
+              >
+                Clear Tasks
+              </button>
+            </div>
+          </div>
+          {/*All Tasks Container*/}
+          <ul className="p-3 border-2 border-indigo-600 rounded-lg space-y-3">
             {tasks.map((task) => (
               <li key={task.id}>
-                <TaskComponent task={task} deleteTask={deleteTask} />
+                <TaskComponent
+                  task={task}
+                  deleteTask={deleteTask}
+                  toggleFavorite={toggleFavorite}
+                  toggleTaskEditing={toggleTaskEditing}
+                  setEditText={setEditText}
+                  editText={editText}
+                  saveEdit={saveEdit}
+                />
               </li>
             ))}
           </ul>
-        </div>
+        </main>
       )}
     </div>
   );
--- src/components/TaskComponent.jsx
+++ src/components/TaskComponent.jsx
@@ -1,21 +1,85 @@
-function TaskComponent({ task, deleteTask }) {
+import { FaStar, FaRegStar, FaTrash, FaRegSave, FaEdit } from "react-icons/fa";
+
+function TaskComponent({
+  task,
+  deleteTask,
+  toggleFavorite,
+  toggleTaskEditing,
+  editText,
+  setEditText,
+  saveEdit,
+}) {
   return (
     <div
-      className={`${task.deleting ? "slide-out-right" : "slide-in-right"}  p-3 m-2 flex  hover:scale-101 hover:border-indigo-500 transition duration-300 ease-in-out  hover:border justify-between bg-zinc-600 rounded
-      hover:shadow-xl`}
+      className={`${
+        task.deleting ? "slide-out-right" : "slide-in-right"
+      } p-0 m-1 flex flex-col sm:flex-row sm:items-center justify-between gap-4 bg-zinc-600 rounded transition duration-300 ease-in-out hover:scale-100 hover:border hover:border-indigo-500 hover:shadow-xl`}
     >
-      <div>
-        <p className=" text-center break-word w-full  ">{task.text}</p>
-      </div>
-      <div className="flex flex-col sm:flex-row gap-2 sm:gap-3">
-        <button className="bg-indigo-600 w-auto p-2 rounded hover:text-yellow-400 transition duration-300">
-          Add To Favorites
+      {task.isEditing ? (
+        <div className="w-full  h-10 flex justify-between focus:outline-1 hover:cursor-text focus:bg-indigo-500">
+          <input
+            placeholder="Edit Text Here"
+            className="min-w-md  p-2"
+            value={editText}
+            onChange={(e) => setEditText(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                saveEdit(task.id);
+              }
+            }}
+            onBlur={() => toggleTaskEditing(task.id)}
+          />
+          <button>
+            <FaRegSave
+              size={20}
+              color="violet"
+              title="Save"
+              className="hover:text-green-600 cursor-pointer"
+              onClick={() => saveEdit(task.id)}
+            />
+          </button>
+        </div>
+      ) : (
+        <div className="w-full  ">
+          <button className="ml-2 hover:cursor-pointer" title="Edit">
+            <FaEdit onClick={() => toggleTaskEditing(task.id)} />
+          </button>
+          <p className="text-left p-2 break-words text-white max-w-full sm:max-w-md">
+            {task.text}
+          </p>
+        </div>
+      )}
+
+      <div className="w-full sm:w-auto flex flex-row justify-start sm:justify-end gap-2 p-2">
+        <button
+          className="bg-indigo-600  px-2 py-2 rounded text-yellow-200 active:opacity-50 transition duration-300 active:scale-95"
+          onClick={() => toggleFavorite(task.id)}
+        >
+          {task.isFavorite ? (
+            <FaStar
+              size={20}
+              color="gold"
+              title="Remove To Favorites"
+              className="hover:text-yellow-600 cursor-pointer"
+            />
+          ) : (
+            <FaRegStar
+              size={20}
+              title="Add To Favorites"
+              className="hover:text-yellow-600 cursor-pointer"
+            />
+          )}
         </button>
         <button
-          className=" ml-3 bg-indigo-600 w-auto p-2 rounded hover:text-red-500 transition duration-300"
+          className="bg-indigo-600  active:opacity-50  text-white px-2 py-2 rounded hover:text-red-500 transition duration-300"
           onClick={() => deleteTask(task.id)}
         >
-          Delete
+          {
+            <FaTrash
+              title="Delete Task"
+              className="text-red-500 hover:text-red-700 cursor-pointer active:opacity-50 transition duration-300"
+            />
+          }
         </button>
       </div>
     </div>
